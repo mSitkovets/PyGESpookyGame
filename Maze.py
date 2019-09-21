a2dcff69869e234ca8106d464ef6f4f3075bc5ef
JustinSo1 <41768142+JustinSo1@users.noreply.github.com>
2019-09-21 16:04:54 -0400

    Fixed incorrect object type comparison

--- Maze.py
+++ Maze.py
@@ -1,6 +1,7 @@
 import pygame
 from PyGE.Objects import ObjectBase
-from PyGE.DisplayMethods import Image
+from PyGE.DisplayMethods import Image
+
 from PyGE.Misc.AlarmClock import AlarmClock
 
 class Maze(ObjectBase):
@@ -8,9 +9,10 @@ class Maze(ObjectBase):
     def oncreate(self):
         self.set_display_method(Image(self.screen, "block"))
     def draw(self):
-        self.draw_to_screen()
+        self.draw_to_screen()
+
     def oncollide(self, obj:'ObjectBase'):
-        if obj.object_type == "Person":
+        if obj.object_type == "Player":
             obj.undo_last_move()  
             
             
