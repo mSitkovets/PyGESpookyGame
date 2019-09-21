b2a806934aff1332ba17f87c97505453a4eaa791
JustinSo1 <41768142+JustinSo1@users.noreply.github.com>
2019-09-21 14:30:21 -0400

    Maze file

---
+++ Maze.py
@@ -0,0 +1,18 @@
+import pygame
+from PyGE.Objects import ObjectBase
+from PyGE.DisplayMethods import Image
+from PyGE.Misc.AlarmClock import AlarmClock
+
+class Maze(ObjectBase):
+    
+    def oncreate(self):
+        self.set_display_method(Image(self.screen, "block"))
+    def draw(self):
+        self.draw_to_screen()
+    def oncollide(self, obj:'ObjectBase'):
+        if obj.object_type == "Person":
+            obj.undo_last_move()  
+            
+            
+        
+    
